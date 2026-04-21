d503578b20d4f4551d309010d550989c0b911bcd
morad <KARKARMORAD29@GMAIL.COM>
2026-04-21 17:37:50 +0200

    Bonus 2 - chiamate fallite

--- main.js
+++ main.js
@@ -6,30 +6,32 @@ async function getDashboardData(query) {
 
     try {
         // Avviamo le richieste in parallelo
-        const [destRes, weatherRes, airportRes] = await Promise.all([
+        const [destRes, weatherRes, airportRes] = await Promise.allSettled([
             fetch(`${baseURL}/destinations?search=${query}`).then(res => res.json()),
             fetch(`${baseURL}/weathers?search=${query}`).then(res => res.json()),
+            //fetch(`https://www.meteofittizio.it`).then(res => res.json()), <--- per il Bonus 2
             fetch(`${baseURL}/airports?search=${query}`).then(res => res.json()),
         ]);
 
-        // Estraiamo il primo elementoda ogni risultato (assumendo che le API restituiscono array)
-        const destination = destRes[0];
-        const weather = weatherRes[0];
-        const airport = airportRes[0];
+        // Estraiamo i dati controllando lo stato ('fulfilled' o 'rejected')
+        // Se la chiamata è fallita, stampiamo l'errore e settiamo a null
+        const destination = results[0].status === 'fulfilled' ? results[0].value[0] : (console.error("Errore destinazione"), null);
+        const weather = results[1].status === 'fulfilled' ? results[1].value[0] : (console.error("Errore meteo"), null);
+        const airport = results[2].status === 'fulfilled' ? results[2].value[0] : (console.error("Errore aeroporto"), null);
 
         //Se uno dei dati manca, restituiamo un messaggio o null
-        if (!destination || !weather || !airport) {
-            //return "Nessun dato trovato per questa città";
-            throw new Error("Dati Incompleti per la città creata.");
-        }
+        // if (!destination || !weather || !airport) {
+        //return "Nessun dato trovato per questa città";
+        //  throw new Error("Dati Incompleti per la città creata.");
+        //}
 
         //Restituamo l'oggetto con le nuove proprietà mappate
         return {
-            city: destination.name,
-            country: destination.country,
-            temperature: weather.temperature,
-            weather: weather.weather_description,
-            airport: airport.name,
+            city: destination ? destination.name : "N/A",
+            country: destination ? destination.country : "N/A",
+            temperature: weather ? weather.temperature : null,
+            weather: weather ? weather.weather_description : null,
+            airport: airport ? airport.name : null,
         };
 
     } catch (error) {
